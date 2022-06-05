5b52f4dab07ef8bf8a5b5ed186acd713761020d2
Lovinaacs <90330784+Lovinaacs@users.noreply.github.com>
2022-06-05 21:37:04 +0700

    Update Main.java

--- bnccGUI/src/bnccGUI/Main.java
+++ bnccGUI/src/bnccGUI/Main.java
@@ -5,11 +5,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.Connection;
-import java.sql.DriverManager;
 
 public class Main extends JFrame implements ActionListener {
 	JPanel p1,p2,p3,p4;
